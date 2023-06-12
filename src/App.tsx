2038b1f2bf15dde1d10184825e7d7c30885ba1ca
ftrbnd <giosalas25@gmail.com>
2023-06-12 14:49:45 -0700

    Create basic implementation of redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { useDispatch, useSelector } from 'react-redux';
 import styles from './styles/App.module.scss';
+import { RootState } from './app/store';
+import { incrementCount } from './features/countSlice';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  const countValue = useSelector((state: RootState) => state.count.value);
+  
+  const handleIncrementCount = () => {
+    dispatch(incrementCount({ value: 1 }))
+  };
+
   return (
     <div className={styles.App}>
-      <h1>Vite + React</h1>
+      <h1>React + TypeScript + Vite + Redux Toolkit + Sass</h1>
+      <h2>Count: {countValue}</h2>
+      <button onClick={handleIncrementCount}>Increment</button>
     </div>
   );
 };
---
+++ src/features/countSlice.ts
@@ -0,0 +1,23 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface Count {
+  value: number;
+}
+
+const initialState: Count = {
+  value: 0
+};
+
+export const countSlice = createSlice({
+    name: "count",
+    initialState,
+    reducers: {
+        incrementCount: (state, action: PayloadAction<Count>) => {
+            state.value += action.payload.value;
+        }
+    },
+});
+
+export const { incrementCount } = countSlice.actions;
+
+export default countSlice.reducer;
