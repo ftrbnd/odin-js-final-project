9637a42083b17a92d66c0913c3428ac90dd669c5
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-06-12 19:32:37 -0700

    Initialize firebase app

---
+++ src/utils/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider, getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC3kcwb2QO-CL9nhJPqsc0MjMyz5x-uV1k",
+  authDomain: "eden-heardle.firebaseapp.com",
+  projectId: "eden-heardle",
+  storageBucket: "eden-heardle.appspot.com",
+  messagingSenderId: "762503136922",
+  appId: "1:762503136922:web:c7fa91063ec89cccf60123"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+export const db = getFirestore(app);
