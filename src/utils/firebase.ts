ea6d0cd8575da1b17a55e346a22d15f7f471c8f6
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-06-12 22:34:19 -0700

    Convert js file to ts

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,14 +1,14 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 import { GoogleAuthProvider, getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC3kcwb2QO-CL9nhJPqsc0MjMyz5x-uV1k",
-  authDomain: "eden-heardle.firebaseapp.com",
-  projectId: "eden-heardle",
-  storageBucket: "eden-heardle.appspot.com",
-  messagingSenderId: "762503136922",
-  appId: "1:762503136922:web:c7fa91063ec89cccf60123"
+  apiKey: 'AIzaSyC3kcwb2QO-CL9nhJPqsc0MjMyz5x-uV1k',
+  authDomain: 'eden-heardle.firebaseapp.com',
+  projectId: 'eden-heardle',
+  storageBucket: 'eden-heardle.appspot.com',
+  messagingSenderId: '762503136922',
+  appId: '1:762503136922:web:c7fa91063ec89cccf60123'
 };
 
 const app = initializeApp(firebaseConfig);
