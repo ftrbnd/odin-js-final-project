5f72d7fb7a299eeb7ffdc3ecaef71f178711392a
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-06-21 13:09:41 -0700

    Remove imports

--- src/components/RulesModal.tsx
+++ src/components/RulesModal.tsx
@@ -1,7 +1,6 @@
 import { Divider, List, ListItemText, Typography } from '@mui/material';
 import Dialog from '@mui/material/Dialog';
 import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import Slide from '@mui/material/Slide';
 import { TransitionProps } from '@mui/material/transitions';
