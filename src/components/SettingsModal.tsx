569d92789646d12c47ee15ca3d4306ab23f4906c
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-07-06 12:18:37 -0700

    Add email verification button

--- src/components/SettingsModal.tsx
+++ src/components/SettingsModal.tsx
@@ -1,12 +1,12 @@
-import { Button, DialogActions, Divider, Link, List, ListItemText } from '@mui/material';
+import { Alert, Button, DialogActions, Divider, Link, List, ListItemText, Snackbar } from '@mui/material';
 import Dialog from '@mui/material/Dialog';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import Slide from '@mui/material/Slide';
 import { TransitionProps } from '@mui/material/transitions';
-import { FC, forwardRef } from 'react';
+import { FC, forwardRef, useState } from 'react';
 import styles from '../styles/SettingsModal.module.scss';
-import { signOut } from 'firebase/auth';
+import { sendEmailVerification, signOut } from 'firebase/auth';
 import { auth } from '../utils/firebase';
 import { useNavigate } from 'react-router-dom';
 
@@ -25,12 +25,49 @@ const Transition = forwardRef(function Transition(
 });
 
 const SettingsModal: FC<IProps> = ({ open, closeModal }) => {
+  const [showSnackbar, setShowSnackbar] = useState<boolean>(false);
+  const [snackbarMessage, setSnackbarMessage] = useState<string>('');
+  const [snackbarSeverity, setSnackbarSeverity] = useState<'success' | 'error'>('success');
+
   const navigate = useNavigate();
 
   const logOut = async () => {
     await signOut(auth);
     closeModal();
     navigate('/');
+  };
+
+  const handleVerifyEmail = async () => {
+    if (!auth.currentUser) return;
+
+    try {
+      await sendEmailVerification(auth.currentUser);
+
+      setShowSnackbar(true);
+      setSnackbarSeverity('success');
+      setSnackbarMessage('Email verification sent!');
+    } catch (e: any) {
+      console.error(e);
+
+      setShowSnackbar(true);
+      setSnackbarSeverity('error');
+
+      switch (e.code) {
+        case 'auth/too-many-requests':
+          setSnackbarMessage('Too many requests, try again later.');
+          break;
+        default:
+          setSnackbarMessage(e.code);
+      }
+    }
+  };
+
+  const handleSnackbarClose = (_event: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setShowSnackbar(false);
   };
 
   return (
@@ -55,6 +92,12 @@ const SettingsModal: FC<IProps> = ({ open, closeModal }) => {
           <div>© 2023 giosalad</div>
         </DialogContent>
         <DialogActions>
+          {auth.currentUser && !auth.currentUser?.emailVerified && (
+            <Button color="warning" onClick={handleVerifyEmail}>
+              Verify your email
+            </Button>
+          )}
+
           {auth.currentUser ? (
             <Button autoFocus onClick={logOut}>
               Log Out
@@ -65,6 +108,11 @@ const SettingsModal: FC<IProps> = ({ open, closeModal }) => {
             </Button>
           )}
         </DialogActions>
+        <Snackbar open={showSnackbar} autoHideDuration={5000} onClose={handleSnackbarClose} anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}>
+          <Alert severity={snackbarSeverity} onClose={handleSnackbarClose} variant="filled">
+            {snackbarMessage}
+          </Alert>
+        </Snackbar>
       </Dialog>
     </div>
   );
